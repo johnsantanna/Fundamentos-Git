0e93f740f963b5703c17150bfaf9f4430937a13b
John Santanna <john.santannas@gmail.com>
2019-07-06 17:50:14 -0300

    Excluiu olá mundo e acrescentou bom dia e otima semana

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,11 +5,8 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Teste");
-
-            for (int i = 0; i < 10; i++) {
-                Console.WriteLine("Oi.................");
-            }
+            Console.WriteLine("Bom dia.");
+            Console.WriteLine("Tenha uma ótima semana!");
 
         }
     }
